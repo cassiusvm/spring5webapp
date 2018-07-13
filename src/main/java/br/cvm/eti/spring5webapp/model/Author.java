ee7d7f7b9e31626b0e001cdb2c82197b64ef97a0
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2018-07-13 00:18:42 -0300

    first commit

---
+++ src/main/java/br/cvm/eti/spring5webapp/model/Author.java
@@ -0,0 +1,31 @@
+package br.cvm.eti.spring5webapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+	private String lastName;
+
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books = new HashSet<>();
+}
