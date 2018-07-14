71a5a704cbbda9f32390e5561a244f50cee246cd
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2018-07-14 15:13:15 -0300

    Sections 2.19 and 2.20
    - Add Publisher Entity
    - Assignment Review of Add Publisher Entity

--- src/main/java/br/cvm/eti/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/br/cvm/eti/spring5webapp/bootstrap/DevBootstrap.java
@@ -6,17 +6,21 @@ import org.springframework.stereotype.Component;
 
 import br.cvm.eti.spring5webapp.model.Author;
 import br.cvm.eti.spring5webapp.model.Book;
+import br.cvm.eti.spring5webapp.model.Publisher;
 import br.cvm.eti.spring5webapp.repositories.AuthorRepository;
 import br.cvm.eti.spring5webapp.repositories.BookRepository;
+import br.cvm.eti.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 	private AuthorRepository authorRepository;
 	private BookRepository bookRepository;
+	private PublisherRepository publisherRepository;
 	
-	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	@Override
@@ -25,8 +29,11 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 	}
 
 	private void initData() {
+		Publisher publisher = new Publisher("foo","bar");
+		publisherRepository.save(publisher);
+		
 		Author eric = new Author("Eric", "Evans");
-		Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+		Book ddd = new Book("Domain Driven Design", "1234", publisher);
 		eric.getBooks().add(ddd);
 		ddd.getAuthors().add(eric);
 		
@@ -34,7 +41,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 		bookRepository.save(ddd);
 
 		Author rod = new Author("Rod", "Johnson");
-		Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox");
+		Book noEJB = new Book("J2EE Development without EJB", "23444", publisher);
 		rod.getBooks().add(noEJB);
 		noEJB.getAuthors().add(rod);
 		
--- src/main/java/br/cvm/eti/spring5webapp/model/Book.java
+++ src/main/java/br/cvm/eti/spring5webapp/model/Book.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,19 +26,21 @@ public class Book {
 	private Long id;
 	private String title;
 	private String isbn;
-	private String publisher;
+	
+	@OneToOne
+	private Publisher publisher;
 
 	@ManyToMany
 	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
 	private Set<Author> authors = new HashSet<>();
 	
-	public Book(String title, String isbn, String publisher) {
+	public Book(String title, String isbn, Publisher publisher) {
 		this.title = title;
 		this.isbn = isbn;
 		this.publisher = publisher;
 	}
 	
-	public Book(String title, String isbn, String publisher, Set<Author> authors) {
+	public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
 		super();
 		this.title = title;
 		this.isbn = isbn;
