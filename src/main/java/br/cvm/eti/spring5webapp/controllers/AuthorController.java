44ff22b0d0b2d149212117c71d26c3782e043e67
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2018-07-14 17:42:07 -0300

    Sections 2.24 and 2.25
    - Assignment - Display List of Authors
    - Assignment Review - Display List of Authors

---
+++ src/main/java/br/cvm/eti/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package br.cvm.eti.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.cvm.eti.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+	private AuthorRepository authorRepository;
+	
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+	}
+	
+	@GetMapping("/authors")
+	public String authors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+		
+		return "authors";
+	}
+}
